f63a214fc1234c34922c45a42c4d7b1621045ff3
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-09-12 01:10:24 +0400

    Update android.sh

--- android.sh
+++ android.sh
@@ -1,13 +1,11 @@
-BOZQURD+="\n ⭑┗━━┫⦀⦙ BozQurd ⦙⦀┣━━┛⭑"
-MESAJ="\n ɃøƶQurđ UserBot Avtomatik deploya xoş gəldiniz"
-MESAJ+="\nBizi seçdiyiniz üçün təşəkkürlər"
-MESAJ+="\nHər hansı bir xəta baş verdikdə ekran şəkli çəkib support qrupuna yazın"
-MESAJ+="\n "
-MESAJ+="\n📣 YENİLƏNMƏ KANALI; @BozQurdResmi"
-MESAJ+="\n🆘 SUPPORT QRUPU; @BozQurdSup"
-MESAJ+="\n🧩 PLUGİN KANALI; @BozQurdPlugin"
-MESAJ+="\n "
-MESAJ+="\n"
+BOZQURD=" __            _               _ "
+BOZQURD+="\n | _ \          |  _  |             | |"
+BOZQURD+="\n | |_/ / _ ____ | | | |_   _ _  | |"
+BOZQURD+="\n | _ \/ _ \_  / | | | | | | | '/ _` |"
+BOZQURD+="\n | |_/ / (_) / /  \ \/' / |_| | | | (_| |"
+BOZQURD+="\n \____/ \___/___|  \_/\_\\__,_|_|  \__,_|"
+BOZQURD+="\n ⭑┗━━┫⦀⦙ 🐺BozQurd Automatic Deployer🐺 ⦙⦀┣━━┛⭑"
+MESAJ+="\n  ⭑┗━━┫⦀⦙ ♻️ Avto Deploy Yüklenir ⦙⦀┣━━┛⭑"
 YARDIM="\n"
 YARDIM+="\n "
 BOSLUK="\n "
@@ -15,8 +13,8 @@ clear
 echo -e $BOZQURD
 echo -e $YARDIM
 echo -e $BOSLUK
-echo "⏳ TERMUX TƏLƏBLƏRİ YENİLƏNİR ⏳"
-echo "⏳ I UPDATE YOUR REQUIREMENTS ⏳"
+echo "✅ TERMUX TƏLƏBLƏRİ YENİLƏNİR "
+echo "✅ I UPDATE YOUR REQUIREMENTS "
 echo -e $BOSLUK
 pkg update -y
 clear
@@ -24,8 +22,8 @@ echo -e $BOZQURD
 echo -e $BOSLUK
 echo -e $MESAJ
 echo -e $BOSLUK
-echo "⌛ CİHAZA PYTHON YAZILIR ⌛"
-echo "⌛ PYTHON IS INSTALLED ON YOUR DEVICE ⌛"
+echo "✅ CİHAZA PYTHON YAZILIR "
+echo "✅ PYTHON IS INSTALLED ON YOUR DEVICE "
 echo -e $BOSLUK
 pkg install python -y
 pip install --upgrade pip
@@ -33,24 +31,24 @@ clear
 echo -e $BOZQURD
 echo -e $MESAJ
 echo -e $BOSLUK
-echo "⌛ GIT QURULUR ⌛"
-echo "⌛ INSTALLING GIT ⌛"
+echo "✅ GIT QURULUR "
+echo "✅ INSTALLING GIT "
 echo -e $BOSLUK
 pkg install git -y
 clear
 echo -e $BOZQURD
 echo -e $MESAJ
 echo -e $BOSLUK
-echo "⌛ TELETHON QURULUR ⌛"
-echo "⌛ INSTALLING TELETHON ⌛"
+echo "✅ TELETHON QURULUR "
+echo "✅ INSTALLING TELETHON "
 echo -e $BOSLUK
 pip install telethon
 clear
 echo -e $BOZQURD
 echo -e $MESAJ
 echo -e $BOSLUK
-echo "⌛ FAYLLAR YÜKLƏNİR ⌛"
-echo "⌛ I DOWNLOAD FİLES ⌛"
+echo "✅ FAYLLAR YÜKLƏNİR "
+echo "✅ I DOWNLOAD FİLES "
 echo -e $BOSLUK
 git clone https://github.com/aykhan026/bozqurdinstaller
 clear
@@ -58,8 +56,8 @@ echo -e $BOZQURD
 echo -e $BOSLUK
 echo -e $MESAJ
 echo -e $BOSLUK
-echo "⌛ TƏLƏBLƏRİ QURURAM..."
-echo "⌛ INSTALLING REQUIREMENTS..."
+echo "✅ TƏLƏBLƏRİ QURURAM..."
+echo "✅ INSTALLING REQUIREMENTS..."
 echo -e $BOSLUK
 cd bozqurdinstaller
 pip install -r requirements.txt
